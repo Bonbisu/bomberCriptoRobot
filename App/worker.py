0a19aeebb59e1edddd50fc95abc7b4c7553b4dd6
thiago.moregola@sib.net.br <Teste@123>
2021-11-09 16:28:10 -0300

    worker balance choose

--- App/worker.py
+++ App/worker.py
@@ -1,3 +1,4 @@
+from random import randrange
 import pyautogui
 import config as c
 import time
@@ -24,35 +25,37 @@ class worker:
                 time.sleep(1)                    
             
             print("click " + str(count) + " in image : " + c.worker_heroes_button)
-            count = 0
-            while count < 2 :
-                
-                count += 1
+            count = 1
+            while count <= 2 :
+                time.sleep(1)
                 lastLocation = 0
                 location = pyautogui.locateAllOnScreen(c.worker_button, confidence=.7)
-                
+
                 for l in location:
-                    
-                    if(pyautogui.locateAllOnScreen(c.worker_button,confidence=.7) is None ):
+                    if pyautogui.locateAllOnScreen(c.worker_stamina_bar,confidence=.99) is None :
                         break
                     
                     if lastLocation <= 0 or lastLocation < l.top  :
                         lastLocation = l.top + 25
                         print("location selected ", l) 
-                        time.sleep(0.5)
                         pyautogui.leftClick(l)
-                        time.sleep(0.5)
                         pyautogui.leftClick(l)
-                        time.sleep(0.5)
+                        
                 lastLocation = 0
-                
-                for i in range(0,9):
-                    pyautogui.leftClick()
-                                
-                    pyautogui.scroll(-15)
-                    pyautogui.scroll(-15)
 
-                    time.sleep(0.5)
+                if count == 1 :
+                    for l in range(0,8):
+                        pyautogui.leftClick()
+                        pyautogui.vscroll(-1)
+                        print("scroll -1") 
+                        time.sleep(1)
+
+                        pyautogui.leftClick()
+                        pyautogui.vscroll(-12)
+                        print("scroll -2", l) 
+                        time.sleep(1)
+                print("count ", count)
+                count += 1 
             count = 0
 
             print("End Worker Select Process") 
@@ -79,4 +82,4 @@ class worker:
             pyautogui.screenshot(c.imagePath + str(datetime.date(2021,11,9)) + "_log.png")
             
             return True
-   # findWorkers() # < teste
+    #findWorkers() # < teste
